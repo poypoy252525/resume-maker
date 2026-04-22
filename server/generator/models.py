eb2a723e3aa5b3f13e5e8c8757357bc20ea9c0fb
poypoy252525 <carljefferson.delfin@gmail.com>
2026-04-22 19:07:31 +0800

    feat: initialize project settings and create Resume model with initial migration

--- server/generator/models.py
+++ server/generator/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
+import uuid
 
-# Create your models here.
+class Resume(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='resumes/')
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
