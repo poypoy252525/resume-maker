79428d4367a91e63bcef823e4e382705ea471e98
poypoy252525 <carljefferson.delfin@gmail.com>
2026-04-22 19:28:20 +0800

    feat: transition from dockerized environment to local development and update Resume model structure

--- local.ps1
+++ local.ps1
@@ -1,10 +1,12 @@
 
 docker compose -f docker-compose.dev.yml up --build -d
 
-# venv/scripts/activate
+cd server
 
-# python manage.py runserver
+venv/scripts/activate
 
-docker compose -f docker-compose.dev.yml exec server clear
-docker compose -f docker-compose.dev.yml exec -it server sh
+python manage.py runserver
+
+# docker compose -f docker-compose.dev.yml exec server clear
+# docker compose -f docker-compose.dev.yml exec -it server sh
 
--- server/generator/models.py
+++ server/generator/models.py
@@ -5,11 +5,11 @@ import uuid
 class Resume(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='resumes/')
+    file = models.FileField(upload_to='resumes/', null=True, blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.file.name
     
