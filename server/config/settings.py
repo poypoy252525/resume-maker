38e2318f4773211fe7ec301a7fd3ebcc439ddb76
poypoy252525 <carljefferson.delfin@gmail.com>
2026-05-16 12:37:59 +0800

    feat: initialize Django project configuration with environment-based settings and essential apps

--- server/config/settings.py
+++ server/config/settings.py
@@ -161,3 +161,11 @@ CORS_ALLOW_ALL_ORIGINS = True # For development
 
 SITE_URL = os.getenv("SITE_URL")
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
